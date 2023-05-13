0cb60b8efc4b781bf37e88b35d0a3d8cf4906dbd
Sony <jsaulhernandez98@gmail.com>
2023-05-12 19:09:25 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import PublicLayout from './layout/PublicLayout';
+
+const App = () => {
+    return <PublicLayout />;
+};
+
+export default App;
---
+++ src/layout/PublicLayout.tsx
@@ -0,0 +1,8 @@
+
+const PublicLayout = () => {
+  return (
+    <div>Hola coyoli</div>
+  )
+}
+
+export default PublicLayout
