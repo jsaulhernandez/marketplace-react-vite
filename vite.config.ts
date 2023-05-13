d8f6fd853a83ed49b8bdb44dd7f2362afcd652eb
Sony <jsaulhernandez98@gmail.com>
2023-05-12 19:43:04 -0600

    tag: updating config for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    resolve: {
+        alias: {
+            '@root': resolve(__dirname, './src'),
+            '@components': resolve(__dirname, './src/components'),
+            '@pages': resolve(__dirname, './src/pages'),
+            '@helpers': resolve(__dirname, './src/helpers'),
+            '@assets': resolve(__dirname, './src/assets'),
+            '@context': resolve(__dirname, './src/context'),
+            '@hooks': resolve(__dirname, './src/hooks'),
+            '@layout': resolve(__dirname, './src/layout'),
+            '@interfaces': resolve(__dirname, './src/interfaces'),
+            '@utils': resolve(__dirname, './src/utils'),
+        },
+    },
+    plugins: [react()],
+    // css: {
+    //     preprocessorOptions: {
+    //         less: {
+    //             modifyVars: {
+    //                 'primary-color': '#d39000',
+    //                 'link-color': '#001a70',
+    //                 'border-radius-base': '4px',
+    //                 'error-color': '#ff859c',
+    //             },
+    //             javascriptEnabled: true,
+    //         },
+    //     },
+    // },
+});
