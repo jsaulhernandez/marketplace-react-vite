520088b2e04dababb87b21ef8be8b74b1f57a357
Sony <jsaulhernandez98@gmail.com>
2023-05-22 11:47:32 -0600

    tag: adding prices list for filters

--- src/constants/Constants.constanst.ts
+++ src/constants/Constants.constanst.ts
@@ -1,1 +1,8 @@
 export const API_URL: string = import.meta.env.VITE_API_URL;
+
+export const PRICES_FILTERS = [
+    '$500 - $1,000',
+    '$1,000 - $1,500',
+    '$1,500 - $2,000',
+    '$2,000 - $2,500',
+];
