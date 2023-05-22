2d371fa6f7edbe23ac2be0ad2b544c72ec9efef1
Sony <jsaulhernandez98@gmail.com>
2023-05-22 11:23:13 -0600

    tag: adding new components for filters

---
+++ src/components/KPCollapse.tsx
@@ -0,0 +1,64 @@
+import { FC } from 'react';
+
+import { Collapse } from 'antd';
+import { CaretRightOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+
+import KPText from './KPText';
+
+const { Panel } = Collapse;
+
+export interface KPCollapseProps {
+    identifier: string;
+    name: string;
+    children: JSX.Element | JSX.Element[];
+}
+
+const KPCollapse: FC<KPCollapseProps> = (props) => {
+    return (
+        <Wrapper
+            bordered={false}
+            collapsible="header"
+            defaultActiveKey={[props.identifier]}
+            expandIconPosition="right"
+            expandIcon={({ isActive }) => (
+                <CaretRightOutlined className="hand" rotate={isActive ? 90 : -90} />
+            )}
+        >
+            <Panel
+                header={
+                    <KPText
+                        text={props.name}
+                        fontWeight={500}
+                        fontSize={16}
+                        textColor="--tertiary-text-color"
+                    />
+                }
+                key={props.identifier}
+            >
+                {props.children}
+            </Panel>
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled(Collapse)`
+    background-color: transparent;
+    border-bottom: 1px solid var(--quaternary-color);
+    border-radius: 0px;
+
+    .ant-collapse-header,
+    .ant-collapse-content-box {
+        padding: 0px !important;
+    }
+
+    .ant-collapse-content-box {
+        padding-top: 10px !important;
+        gap: 10px !important;
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 10px;
+    }
+`;
+
+export default KPCollapse;
---
+++ src/components/KPItemFilter.tsx
@@ -0,0 +1,80 @@
+import { FC } from 'react';
+
+import { Checkbox } from 'antd';
+import { CloseCircleOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+
+import KPText from './KPText';
+
+type typeFilter = 'category' | 'amount' | 'tag';
+
+export interface KPItemFilterProps {
+    type?: typeFilter;
+    onClick?: (value?: string | boolean) => void;
+    label?: string;
+    value?: string;
+    active?: boolean;
+}
+
+const KPItemFilter: FC<KPItemFilterProps> = (props) => {
+    if (props.type === 'tag')
+        return (
+            <TagWrapper className="flex items-center g-10 p-1">
+                <KPText text="Hola como estas xdxd" />
+                <CloseCircleOutlined
+                    className="hand closable"
+                    onClick={() => props.onClick && props.onClick()}
+                />
+            </TagWrapper>
+        );
+
+    if (props.type === 'amount')
+        return (
+            <Amount
+                className="flex items-center p-1 hand"
+                onClick={() => props.onClick && props.onClick()}
+                active={true}
+            >
+                <KPText text="$100 - $200" fontWeight={500} />
+            </Amount>
+        );
+
+    return (
+        <Category className="flex items-center g-10">
+            <Checkbox
+                onChange={(e) => props.onClick && props.onClick(e.target.checked)}
+            />
+            <KPText text="AirPods" />
+        </Category>
+    );
+};
+
+const Category = styled.div``;
+
+const TagWrapper = styled.div`
+    border-radius: 5px;
+    /* border: 1px solid var(--quaternary-color); */
+    background-color: var(--secondary-color);
+    box-shadow: -5px -5px 9px rgba(255, 255, 255, 0.45),
+        5px 5px 9px rgba(94, 104, 121, 0.3);
+
+    .closable {
+        color: var(--secondary-text-color);
+    }
+`;
+
+const Amount = styled.div<{
+    active?: boolean;
+}>`
+    border-radius: 5px;
+    border: 1px solid
+        ${({ active }) => (active ? 'var(--primary-color)' : 'var(--quaternary-color)')};
+    background-color: ${({ active }) => (active ? '#CEE1F9' : 'var(--secondary-color)')};
+
+    p {
+        color: ${({ active }) =>
+            active ? 'var(--primary-color)' : 'var(--secondary-text-color)'};
+    }
+`;
+
+export default KPItemFilter;
