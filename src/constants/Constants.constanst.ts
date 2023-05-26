453686f5302dfb52e2f328e91e4e533ad5b3d92c
Sony <jsaulhernandez98@gmail.com>
2023-05-26 12:55:53 -0600

    tag: update prices for filters

--- src/constants/Constants.constanst.ts
+++ src/constants/Constants.constanst.ts
@@ -1,8 +1,8 @@
 export const API_URL: string = import.meta.env.VITE_API_URL;
 
 export const PRICES_FILTERS = [
-    '$500 - $1,000',
-    '$1,000 - $1,500',
-    '$1,500 - $2,000',
-    '$2,000 - $2,500',
+    { label: '$500 - $1,000', value: '500,1000', active: false },
+    { label: '$1,000 - $1,500', value: '1000,1500', active: false },
+    { label: '$1,500 - $2,000', value: '1500,2000', active: false },
+    { label: '$2,000 - $2,500', value: '2000,2500', active: false },
 ];
