e91914b97576b6f3287303897fa9eb0983f6f4fa
Sony <jsaulhernandez98@gmail.com>
2023-05-31 15:21:41 -0600

    tag: adding support for select color

--- src/components/products/KPDotsColor.tsx
+++ src/components/products/KPDotsColor.tsx
@@ -1,15 +1,32 @@
 import { FC } from 'react';
-import styled from 'styled-components';
+import styled, { CSSProperties } from 'styled-components';
 
 export interface KPDotsColorProps {
     colors?: string[];
+    cssProperties?: CSSProperties;
+    className?: string;
+    isHover?: boolean;
+    active?: string;
+    onClick?: (value: string) => void;
 }
 
-const KPDotsColor: FC<KPDotsColorProps> = ({ colors }) => {
+const KPDotsColor: FC<KPDotsColorProps> = ({
+    isHover = false,
+    active = '',
+    ...props
+}) => {
     return (
         <Wrapper className="flex flex-row flex-wrap mt-1 mb-1">
-            {colors?.map((x, i) => (
-                <Dot color={x} key={i} />
+            {props.colors?.map((x, i) => (
+                <Dot
+                    color={x}
+                    key={i}
+                    style={props.cssProperties}
+                    className={`${props.className ? props.className : ''} ${
+                        isHover ? 'isHover hand' : ''
+                    } ${active === x ? 'active' : ''} `}
+                    onClick={() => props.onClick && props.onClick(x)}
+                />
             ))}
         </Wrapper>
     );
@@ -17,6 +34,18 @@ const KPDotsColor: FC<KPDotsColorProps> = ({ colors }) => {
 
 const Wrapper = styled.div`
     gap: 10px;
+
+    .isHover {
+        position: relative;
+        width: 25px !important;
+        height: 25px !important;
+    }
+
+    .isHover:hover,
+    .active {
+        border: 1px solid var(--secondary-color);
+        outline: 2px solid var(--primary-color);
+    }
 `;
 
 const Dot = styled.div<{
