6ab3a7b4bf1e5d12c0703ec27180112dd2c66af9
Sony <jsaulhernandez98@gmail.com>
2023-05-31 13:07:17 -0600

    tag: creating custom breadcrumb

---
+++ src/components/KPBreadcrumb.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { Breadcrumb } from 'antd';
+import { ItemType } from 'antd/es/breadcrumb/Breadcrumb';
+import { RightOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+
+import { children } from '@root/routes';
+
+export interface KPBreadcrumbProps {
+    titles?: ItemType[];
+}
+
+const KPBreadcrumb: FC<KPBreadcrumbProps> = (props) => {
+    const { pathname } = useLocation();
+
+    const buildingItems = (): ItemType[] => {
+        let items: ItemType[] = [];
+        const getItem = children.find((r) => r.path === pathname);
+
+        if (!getItem) return items;
+
+        items.push({ title: getItem.title });
+
+        items = items.concat(props?.titles ?? []);
+
+        return items;
+    };
+
+    return <Wrapper separator={<RightOutlined />} items={buildingItems()} />;
+};
+
+const Wrapper = styled(Breadcrumb)`
+    .ant-breadcrumb-separator {
+        margin-inline: 25px;
+    }
+`;
+
+export default KPBreadcrumb;
