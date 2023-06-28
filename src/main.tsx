e45036ec6c55184cd79ec11a0a358ac6431eaddc
Sony <jsaulhernandez98@gmail.com>
2023-06-28 17:45:50 -0600

    tag: adding days library

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,9 @@ import es_ES from 'antd/es/locale/es_ES';
 import moment from 'moment';
 import 'moment/dist/locale/es';
 
+import * as dayjs from 'dayjs';
+import 'dayjs/locale/es'; // load on demand
+
 import App from './App.tsx';
 import './style.css';
 
@@ -17,6 +20,7 @@ import CartProvider from '@context/CartContext.context.tsx';
 import { UserProvider } from '@context/UserContext.context.tsx';
 
 moment.locale('es');
+dayjs.locale('es');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
