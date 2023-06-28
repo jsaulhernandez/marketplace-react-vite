eaad05f89095148d5771c35a0ad58a329edbae2f
Sony <jsaulhernandez98@gmail.com>
2023-06-28 15:21:59 -0600

    tag: updating routes

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const SignUp = () => {
+    return (
+        <Wrapper className="flex flex-column justify-center items-center">SignUp</Wrapper>
+    );
+};
+
+const Wrapper = styled.div`
+    min-width: 100vw;
+    min-height: 100vh;
+`;
+
+export default SignUp;
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,11 +4,12 @@ import Layout from '@layout/Layout';
 
 import Home from '@pages/Home';
 import Cart from '@pages/Cart';
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
 
-const RoutesList = [
-    { path: '/kplace/home', element: <Home />, title: 'Inicio' },
-    { path: '/kplace/cart', element: <Cart />, title: 'Carrito' },
-];
+const PrivateRoutes = [{ path: '/kplace/cart', element: <Cart />, title: 'Carrito' }];
+
+const RoutesList = [{ path: '/kplace/home', element: <Home />, title: 'Inicio' }];
 
 const Routes = [
     { path: '*', element: <Navigate to="/kplace/home" replace /> },
@@ -17,6 +18,8 @@ const Routes = [
         element: <Layout />,
         children: RoutesList,
     },
+    { path: '/auth/login', element: <LogIn />, title: 'Inicio de sesión' },
+    { path: '/auth/signup', element: <SignUp />, title: 'Registrarse en KPlace' },
 ];
 
-export { Routes, RoutesList };
+export { Routes, RoutesList, PrivateRoutes };
